50bf5d5546c26a9b31928f797e2bee1594db4482
Darshan Ambule <darshan.ambule.da@gmail.com>
2024-11-30 23:15:17 +0530

    add streamlit config

--- main.py
+++ main.py
@@ -22,13 +22,27 @@ class_dict = json.load(open(class_names_path))
 img_size = IMG_SIZE
 #preprocessiong
 def preprocess_image(image_path):
+  """
+  Preprocess an image given the image path.
+
+  Parameters
+  ----------
+  image_path : str
+    The path to the image file.
+
+  Returns
+  -------
+  numpy.ndarray
+    The preprocessed image in numpy array format.
+
+  """
   img = tf.keras.preprocessing.image.load_img(image_path,target_size = (img_size,img_size))
   img = tf.keras.preprocessing.image.img_to_array(img)
   img = img/255.0
   img = np.expand_dims(img,axis = 0)
   return img
 
-def predict_image(image_path):
+def predict_image(model,image_path,class_dict):
   """
   Predict the class of an image given the image path.
 
@@ -48,3 +62,21 @@ def predict_image(image_path):
   predicted_class_index = np.argmax(prediction)
   predicted_class = class_dict[predicted_class_index]
   return predicted_class
+
+# Streamlit app
+st.title("Plant Disease Detection")
+uploaded_image = st.file_uploader("Upload an image...", type=["jpg", "jpeg", "png"])
+
+if uploaded_image is not None:
+    image = Image.open(uploaded_image)
+    col1, col2 = st.columns(2)
+
+    with col1:
+        resized_img = image.resize((img_size, img_size))
+        st.image(resized_img)
+
+    with col2:
+        if st.button('Classify'):
+            # Preprocess the uploaded image and predict the class
+            prediction = predict_image(model, uploaded_image, class_dict)
+            st.success(f'Prediction: {str(prediction)}')
