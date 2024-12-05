4d21a31a194ca968d32034391ae488cb63de15d7
Darshan Ambule <darshan.ambule.da@gmail.com>
2024-12-06 00:10:56 +0530

    add st changes and pred improvements

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from PIL import Image
 import numpy as np
 import tensorflow as tf
 import streamlit as st
-from PlantDisease.ModelParams import IMG_SIZE, BATCH_SIZE
+from PlantDisease.ModelParams import IMG_SIZE, BATCH_SIZE, PREDICTION_THRESHOLD
 
 # Define the current directory
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -62,11 +62,24 @@ def predict_image(model,image_path,class_dict):
   img = preprocess_image(image_path)
   prediction = model.predict(img,verbose = 0)
   predicted_class_index = np.argmax(prediction).astype(str)
-  predicted_class = class_dict[predicted_class_index]
-  return predicted_class
+  confidence = np.max(prediction)
+  
+  if confidence < PREDICTION_THRESHOLD:
+    predicted_class = "Cannot be predicted"
+  else:
+    predicted_class = class_dict[predicted_class_index]
+  return predicted_class,confidence
 
 # # Streamlit app
 st.title("Plant Disease Detection")
+st.markdown("""
+This application detects plant diseases using a **custom ResNet-34 model** built from scratch with TensorFlow. The model is trained on the [PlantVillage Dataset](https://www.kaggle.com/datasets/abdallahalidev/plantvillage-dataset), which contains labeled images of healthy and diseased plant leaves. """)
+
+st.markdown("""
+### How to Use:
+1. Upload a leaf image (jpg, jpeg, or png).
+2. Click on "Classify" to predict whether the leaf is healthy or diseased.
+""")
 uploaded_image = st.file_uploader("Upload an image...", type=["jpg", "jpeg", "png"])
 
 if uploaded_image is not None:
@@ -80,5 +93,14 @@ if uploaded_image is not None:
     with col2:
         if st.button('Classify'):
             # Preprocess the uploaded image and predict the class
-            prediction = predict_image(model, uploaded_image, class_dict)
-            st.success(f'Prediction: {str(prediction)}')
+            prediction,confidence = predict_image(model, uploaded_image, class_dict)
+            if confidence < PREDICTION_THRESHOLD:
+                st.error(f'Prediction: {str(prediction)}')
+            else:
+                st.success(f'Prediction: {str(prediction)}')
+                st.write(f'Confidence: {confidence:.2f}')
+
+st.markdown("""
+## Model Architecture:
+The model is built using the **ResNet-34 architecture**, which utilizes **residual blocks** with identity and convolutional shortcuts. This architecture allows the model to effectively learn deeper representations, which is crucial for complex image classification tasks like plant disease detection.""")
+st.image("Ref\Model Architecture.png", caption="ResNet-34 Model Architecture", use_container_width =True)
